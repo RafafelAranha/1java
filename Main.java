56cb51404abb767659f7228af633020aa23cc5c4
unknown <tad-rafaelmarin@ucpparana.edu.br>
2024-02-16 22:04:50 -0300

    primeiro

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        Product uva = new Product();
+        uva.setName("Uva");
+        System.out.println(uva.getName());
+    }
+}
---
+++ Product.java
@@ -0,0 +1,13 @@
+public class Product {
+    public String name;
+    public String description;
+    public Double wight;
+    public Double price;
+
+    public void setName(String productName){
+    name = productName;
+}
+
+    public String getName(){        return name;
+    }
+}
